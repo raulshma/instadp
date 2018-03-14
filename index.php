8b322f2f5d7424eb6a435f2bc4772ac7fde7c304
succkarz <succkarz@gmail.com>
2018-03-15 00:51:34 +0530

    Added Footer and description text.

--- index.php
+++ index.php
@@ -1,60 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/skeleton.css">
+
+    <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta charset="utf-8">
+    <title>Insta DP Download</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+
+    <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <link rel="stylesheet" href="css/normalize.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/skeleton.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/jquery.fancybox.min.css">
-    <title>Instagram DP</title>
-</head>
-<body>
-<div class="table-container">
-<header>
-    <div class="full-width-container">
-        <div class="active" style="padding-top: 5px; padding-bottom: 5px;">
-        <a class="nodec right" href="http://instadp.ml">
-            <span class="center-text">INSTADP</span>
-        </a>
-        <a class="nodec left" href="#">
-        <img class="center-text navimgsize" src="images/social_github.png"></img>
-            <span class="center-text">Github</span>
-        </a>
-        <a class="nodec left" href="#">
-        <img class="center-text navimgsize" src="images/social_facebook.png"></img>
-            <span class="center-text">Facebook</span>
-        </a>
-        <a class="nodec left" href="#">
-        <img class="center-text navimgsize" src="images/social_instagram.png"></img>
-            <span class="center-text">Instagram</span>
-        </a>
-        <a class="nodec left" href="#">
-        <img class="center-text navimgsize" src="images/social_youtube.png"></img>
-            <span class="center-text">Youtube</span>
-        </a>
-        <div style="clear: both;"></div>
-        </div>
-        <div>
-        <select> 
-            <option value="http://instadp.ml" selected="selected">INSTADP</option> 
-            <option value="#">Github</option> 
-            <option value="#">Facebook</option> 
-            <option value="#">Instagram</option> 
-            <option value="#">Youtube</option> 
-        </select>
-        </div>
-    </div>
-</header>
 
-<div style="width:100%;text-align:center;">
-<div class="containermid">
-    <form style="padding-top:10px;" action="index.php" method="post">
+    <!-- Scripts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+
+    <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <link rel="icon" type="image/png" href="dist/images/favicon.png">
+
+</head>
+
+<body class="code-snippets-visible">
+
+    <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="container" style="text-align:center;">
+        <section style="text-align: center;">
+            <h1 class="title">Instadp</h1>
+        </section>
+        <div class="navbar-spacer"></div>
+        <nav class="navbar">
+            <div class="container">
+                <ul class="navbar-list">
+                    <li class="navbar-item">
+                        <a class="navbar-link" href="#" data-popover="#codeNavPopover">Social</a>
+                        <div id="codeNavPopover" class="popover">
+                            <ul class="popover-list">
+                                <li class="popover-item">
+                                    <a class="popover-link" href="#grid">Instagram</a>
+                                </li>
+                                <li class="popover-item">
+                                    <a class="popover-link" href="#typography">Facebook</a>
+                                </li>
+                                <li class="popover-item">
+                                    <a class="popover-link" href="#buttons">Deviantart</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="navbar-item">
+                        <a class="navbar-link" href="#" data-popover="#moreNavPopover">Other</a>
+                        <div id="moreNavPopover" class="popover">
+                            <ul class="popover-list">
+                                <li class="popover-item">
+                                    <a class="popover-link" href="https://github.com/succkarz/instadp">Source</a>
+                                </li>
+                                <li class="popover-item">
+                                    <a class="popover-link" href="https://github.com/succkarz/instadp/blob/master/README.md">About</a>
+                                </li>
+                                <li class="popover-item">
+                                    <a class="popover-link" href="https://github.com/succkarz/instadp/blob/master/LICENSE">License</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="navbar-spacer"></div>
+        <div style="padding-top:20px;">
+        <p>Ever wanted to see someone's Instagram profile picture in full size but didn't know how? Instadp is a free to use service that allows you to see anyone's Instagram's profile picture in full size. Works on any account - even private profiles! Bookmark our webpage to see Instagram Profile Pictures on your phone as well.
+        <form style="padding-top:10px;" action="index.php" method="post">
         <input type="text" name="name" placeholder="IG username" required/>
-        <input style="background-color:#3498BD;color:#FFFFFF;" class="button" value="Submit" type="submit"/> 
-    </form>
+        <input class="button" value="Submit" type="submit"/> 
+    </form>    
+    </div>
+        
 <?php
 error_reporting(0);
 if(isset($_POST['name'])){
@@ -80,20 +119,12 @@ echo "<br/>Followers:<b> $followers</b>";
     echo "Username does not exist.";
 }}
 ?>
-</div>
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="js/jquery.fancybox.min.js"></script>
-<!-- Page Footer Layout
-           –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <div class="table-block">
+    </div>
+    <div id="footer">
         <div class="container">
-            <footer id="footer" class="twelve columns nodec">
-                 Instadp Ⓒ 2018<br/>
-                 Not affiliated with or endorsed by Instagram.
-            </footer>
-        </div>
-    </div> <!-- end footer div.container -->
+    © Instadp 2018<br>
+We are not affiliated with or endorsed by Instagram.
 </div>
+    </div>
 </body>
 </html>
