8e0104120ffa27ebb9e2ea4edabe192f57bb4b76
raulshma <succkarz@gmail.com>
2018-04-08 09:05:19 +0530

    Changes Simple

--- index.php
+++ index.php
@@ -1,138 +1,118 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-
-<!-- Basic Page Needs
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<meta charset="utf-8">
-<title>Insta DP Download</title>
-<meta name="author" content="raul">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="See and download anyone Instagram Profile Picture for free in full size!">
-<meta name="keywords" content="Instagram Profile Picture, InstaDP, instagram, display picture, display, picture, view, profile, picture,anyone Instagram, picture in full size,account even private,anyone instagram profile,instadp">
-<meta name="robots" content="index, follow">
-<meta http-equiv="Cache-control" content="no-cache">
-<meta name="revisit-after" content="3 month">
-<meta name="theme-color" content="#3A3D3D"/>
-
-<!-- Mobile Specific Metas
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<!-- FONT
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
-
-<!-- CSS
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/skeleton.css">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" />
-
-<!-- Scripts
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<!--Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116092546-1"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js">
-</script>
-<script src="js/pace.js"></script>
-<script src="js/custom.js"></script>
-
-<!-- Favicon
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-<link rel="icon" type="images/jpg" href="images/favicon.png">
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-116092546-1');
-</script>
-    <!-- Favicon
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <link rel="icon" type="images/jpg" href="images/favicon.png">
-  </head>
-  <body class="code-snippets-visible>
-    <!-- Primary Page Layout
-–––––––––––––––––––––––––––––––––––––––––––––––––– -->
-    <div class="container" style="text-align:center;">
-      <section style="text-align: center;">
-        <h1 class="title"><a class="notdec" href="index.php">Instadp</a>
-        </h1>
-      </section>
-      <div class="navbar-spacer">
+   <head>
+      <!-- Basic Page Needs
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <meta charset="utf-8">
+      <title>Instagram DP Download</title>
+      <meta name="author" content="raul">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+      <link rel='dns-prefetch' href='//cdnjs.cloudflare.com' />
+      <link rel='dns-prefetch' href='//ajax.googleapis.com' />
+      <meta name="description" content="See and download anyone Instagram Profile Picture for free in full size">
+      <meta name="keywords" content="Instagram Profile Picture, InstaDP, instagram, display picture, display, picture, view, profile, picture,anyone Instagram, picture in full size,account even private,anyone instagram profile,instadp">
+      <meta property="og:title" content="Instagram DP Download" />
+      <meta property="og:type" content="article" />
+      <meta property="og:url" content="http://instadp.ml/" />
+      <meta property="og:image" content="http://instadp.ml/images/screen.png" />
+      <meta property="og:description" content="See and download anyone Instagram Profile Picture for free in full size" /> 
+      <meta property="og:site_name" content="Instagram DP Download" />
+      <meta name="robots" content="index, follow">
+      <meta name="revisit-after" content="1 month">
+      <meta name="theme-color" content="#3A3D3D"/>
+      <!-- Mobile Specific Metas
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- FONT
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <link href='//fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
+      <!-- CSS
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <link rel="stylesheet" href="css/normalize.css">
+      <link rel="stylesheet" href="css/skeleton.css">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css" />
+      <!-- Scripts
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <!-- Favicon
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <link rel="icon" type="images/jpg" href="images/favicon.png">
+      <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         
+         gtag('config', 'UA-116092546-1');
+      </script>
+      <!-- Favicon
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <link rel="icon" type="images/jpg" href="images/favicon.png">
+   </head>
+   <body class="code-snippets-visible">
+      <!-- Primary Page Layout
+         –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+         <div class="container" style="text-align:center;">
+         <section style="text-align: center;">
+            <h1 class="title"><a class="notdec" href="index.php">Instadp</a>
+            </h1>
+         </section>
+         <div class="navbar-spacer">
+         </div>
+         <nav class="navbar">
+            <div class="container">
+               <ul class="navbar-list">
+                  <li class="navbar-item">
+                     <a class="navbar-link" href="https://github.com/succkarz/instadp">Source
+                     </a>
+                  </li>
+                  <li class="navbar-item">
+                     <a class="navbar-link" href="https://github.com/succkarz/instadp/blob/master/README.md">About
+                     </a>
+                  </li>
+                  <li class="navbar-item">
+                     <a class="navbar-link" href="https://github.com/succkarz/instadp/blob/master/LICENSE">License
+                     </a>
+                  </li>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+         <div class="navbar-spacer">
+         </div>
+         <div class="container">
+            <form style="padding-top:10px;" method="post">
+               <input id="name" type="text" name="uname" placeholder="IG username" required autofocus/>
+               <input id="submit" class="button" name="Submit" value="Submit" type="button"/>
+            </form>
+            <div class="sk-folding-cube loading">
+               <div class="sk-cube1 sk-cube"></div>
+               <div class="sk-cube2 sk-cube"></div>
+               <div class="sk-cube4 sk-cube"></div>
+               <div class="sk-cube3 sk-cube"></div>
+            </div>
+            <div class="container" id="response"></div>
+            <p>
+               Ever wanted to see someone's Instagram profile picture in 
+               full size but didn't know how? Instadp is a free to use service 
+               that allows you to see anyone's Instagram's profile picture in full size. Works on any account 
+               - even private profiles! Bookmark our webpage to see 
+               Instagram Profile Pictures on your phone as well.
+            </p>
+         </div>
       </div>
-      <nav class="navbar">
-        <div class="container">
-          <ul class="navbar-list">
-            <li class="navbar-item">
-              <a class="navbar-link" href="#" data-popover="#codeNavPopover">Social
-              </a>
-              <div id="codeNavPopover" class="popover">
-                <ul class="popover-list">
-                  <li class="popover-item">
-                    <a class="popover-link" href="#grid">Instagram
-                    </a>
-                  </li>
-                  <li class="popover-item">
-                    <a class="popover-link" href="#typography">Facebook
-                    </a>
-                  </li>
-                  <li class="popover-item">
-                    <a class="popover-link" href="#buttons">Deviantart
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="navbar-item">
-              <a class="navbar-link" href="#" data-popover="#moreNavPopover">Other
-              </a>
-              <div id="moreNavPopover" class="popover">
-                <ul class="popover-list">
-                  <li class="popover-item">
-                    <a class="popover-link" href="https://github.com/succkarz/instadp">Source
-                    </a>
-                  </li>
-                  <li class="popover-item">
-                    <a class="popover-link" href="https://github.com/succkarz/instadp/blob/master/README.md">About
-                    </a>
-                  </li>
-                  <li class="popover-item">
-                    <a class="popover-link" href="https://github.com/succkarz/instadp/blob/master/LICENSE">License
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div class="navbar-spacer">
-      </div>
-      <div class="container" style="padding-top:20px;">
-        <p>Ever wanted to see someone's Instagram profile picture in full size but didn't know how? Instadp is a free to use service that allows you to see anyone's Instagram's profile picture in full size. Works on any account - even private profiles! Bookmark our webpage to see Instagram Profile Pictures on your phone as well.
-        <form style="padding-top:10px;" method="post">
-          <input id="name" type="text" name="uname" placeholder="IG username" required autofocus/>
-          <input id="submit" class="button" name="Submit" value="Submit" type="button"/>
-        </form>
-        <div class="sk-folding-cube loading">
-          <div class="sk-cube1 sk-cube"></div>
-          <div class="sk-cube2 sk-cube"></div>
-          <div class="sk-cube4 sk-cube"></div>
-          <div class="sk-cube3 sk-cube"></div>
-        </div>
-        <div class="container" id="response"></div>   
-      </div>
-    </div>
-    <footer id="foot">
-      <div class="container">
-        © Instadp 2018
-        <br>
-        We are not affiliated with or endorsed by Instagram.
-      </div>
-    </footer>
-  </body>
+      <footer id="foot">
+         <div class="container">
+            © Instadp 2018
+            <br>
+            We are not affiliated with or endorsed by Instagram.
+         </div>
+      </footer>
+      <!--Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116092546-1"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+      <script src="js/pace.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
 </html>
